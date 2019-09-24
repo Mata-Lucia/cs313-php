fc8d0d970862239879dfa4b6538c0b3a713b5108
luciamata5 <mat18013@byui.edu>
2019-09-24 16:48:32 -0600

    update

---
+++ web/assignments.php
@@ -0,0 +1,51 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <title>CS 313 Lucia Mata</title>
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+			<div class="container">
+         <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Menu</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="assignments.php">Assignments</a></li>
+				</ul>
+      </div>
+      </div>
+    </nav>
+    </header>
+    <main>
+      <div class="introdiv">
+        <h1>Assignments</h1>
+        <h2>Coming Soon!</h2>
+      </div> 
+        <div class="linkdiv">
+            <a href="assignments.php">Assignments</a>
+            <img src="images/code.png" alt="codeicon" class="icon">
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</body>
+</html>
