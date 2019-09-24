03ca498812ce1f7129c61421536ba9edd69eeb8d
luciamata5 <mat18013@byui.edu>
2019-09-24 15:25:37 -0600

    testing

--- web/index.php
+++ web/index.php
@@ -1,59 +1,37 @@
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link type="text/css" rel="stylesheet" href="index.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-    <title>CS 313 Lucia Mata</title>
-</head>
-<body>
-    <header>
-    <!--Navbar-->
-<nav class="navbar navbar-light light-blue lighten-4">
+<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+			<div class="container">
+         <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Menu</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
 
-<!-- Navbar brand -->
-<a class="navbar-brand" href="#">Navbar</a>
-
-<!-- Collapse button -->
-<button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent1"
-  aria-controls="navbarSupportedContent1" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-blue-text"><i
-      class="fas fa-bars fa-1x"></i></span></button>
-
-<!-- Collapsible content -->
-<div class="collapse navbar-collapse" id="navbarSupportedContent1">
-
-  <!-- Links -->
-  <ul class="navbar-nav mr-auto">
-    <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Features</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#">Pricing</a>
-    </li>
-  </ul>
-  <!-- Links -->
-
-</div>
-<!-- Collapsible content -->
-
-</nav>
-<!--/.Navbar-->
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="assignments.php">Assignments</a></li>
+				</ul>
+			</div>
+		</nav>
+  </div>
 
     </header>
     <main>
         <h1>Hello</h1>
         <h2>My name is Lucia</h2>
+        <img src="images/lucia.png" alt="lucia" class="me">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque vulputate interdum. Nam accumsan tincidunt ligula at egestas. Donec eget metus at magna lacinia accumsan. Donec posuere lacus erat, eu sodales lorem scelerisque ac. Pellentesque tempus lacus nibh, a vehicula turpis mattis eget. Mauris varius rhoncus libero, quis faucibus est. In augue sem, tempor non rutrum consectetur, porta at est. Vivamus id urna ut sem rhoncus convallis. Duis et pulvinar enim.
 
         Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam cursus mollis leo pharetra pulvinar. Donec eu augue vitae eros lacinia tincidunt. Vivamus pulvinar erat sed consectetur maximus. Suspendisse egestas neque eu urna gravida finibus. Morbi volutpat quam ut erat consequat blandit sagittis et nisl. Sed laoreet metus quis dignissim auctor. Nam sit amet odio sed sapien dignissim varius. Duis pellentesque vulputate diam, egestas lobortis neque vestibulum eu.</p>
-        <img src="images/lucia.png" alt="lucia">
-        <div>
+        <div class="linkdiv">
             <a href="assignments.php">Assignments</a>
-            <img src="images/code.png" alt="codeicon">
+            <img src="images/code.png" alt="codeicon" class="icon">
         </div>
     </main>
     <footer>
