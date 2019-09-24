ff578e2ddd9142c695bef1cf4226ddcb6c65b0b4
luciamata5 <mat18013@byui.edu>
2019-09-24 15:31:33 -0600

    update

--- web/index.php
+++ web/index.php
@@ -1,6 +1,15 @@
 <html lang="en">
 <head>
-<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <title>CS 313 Lucia Mata</title>
+</head>
+<body>
+    <header>
+    <nav class="navbar navbar-inverse navbar-static-top" role="navigation">
 			<div class="container">
          <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
@@ -25,7 +34,7 @@
     <main>
         <h1>Hello</h1>
         <h2>My name is Lucia</h2>
-        <img src="images/lucia.png" alt="lucia" class="me">
+        <img src="images/lucia.png" alt="lucia">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque vulputate interdum. Nam accumsan tincidunt ligula at egestas. Donec eget metus at magna lacinia accumsan. Donec posuere lacus erat, eu sodales lorem scelerisque ac. Pellentesque tempus lacus nibh, a vehicula turpis mattis eget. Mauris varius rhoncus libero, quis faucibus est. In augue sem, tempor non rutrum consectetur, porta at est. Vivamus id urna ut sem rhoncus convallis. Duis et pulvinar enim.
 
         Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam cursus mollis leo pharetra pulvinar. Donec eu augue vitae eros lacinia tincidunt. Vivamus pulvinar erat sed consectetur maximus. Suspendisse egestas neque eu urna gravida finibus. Morbi volutpat quam ut erat consequat blandit sagittis et nisl. Sed laoreet metus quis dignissim auctor. Nam sit amet odio sed sapien dignissim varius. Duis pellentesque vulputate diam, egestas lobortis neque vestibulum eu.</p>
