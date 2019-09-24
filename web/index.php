c9a61305bce9e0cf16c3b8b3f54b92e6cc5c3b29
luciamata5 <mat18013@byui.edu>
2019-09-24 15:49:26 -0600

    testing css

--- web/index.php
+++ web/index.php
@@ -35,9 +35,8 @@
         <h1>Hello</h1>
         <h2>My name is Lucia</h2>
         <img src="images/lucia.png" alt="lucia">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque vulputate interdum. Nam accumsan tincidunt ligula at egestas. Donec eget metus at magna lacinia accumsan. Donec posuere lacus erat, eu sodales lorem scelerisque ac. Pellentesque tempus lacus nibh, a vehicula turpis mattis eget. Mauris varius rhoncus libero, quis faucibus est. In augue sem, tempor non rutrum consectetur, porta at est. Vivamus id urna ut sem rhoncus convallis. Duis et pulvinar enim.
-
-        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam cursus mollis leo pharetra pulvinar. Donec eu augue vitae eros lacinia tincidunt. Vivamus pulvinar erat sed consectetur maximus. Suspendisse egestas neque eu urna gravida finibus. Morbi volutpat quam ut erat consequat blandit sagittis et nisl. Sed laoreet metus quis dignissim auctor. Nam sit amet odio sed sapien dignissim varius. Duis pellentesque vulputate diam, egestas lobortis neque vestibulum eu.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque vulputate interdum. Nam accumsan tincidunt ligula at egestas. Donec eget metus at magna lacinia accumsan. Donec posuere lacus erat, eu sodales lorem scelerisque ac. Pellentesque tempus lacus nibh, a vehicula turpis mattis eget. Mauris varius rhoncus libero, quis faucibus est. In augue sem, tempor non rutrum consectetur, porta at est. Vivamus id urna ut sem rhoncus convallis. Duis et pulvinar enim.</p>
+        <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam cursus mollis leo pharetra pulvinar. Donec eu augue vitae eros lacinia tincidunt. Vivamus pulvinar erat sed consectetur maximus. Suspendisse egestas neque eu urna gravida finibus. Morbi volutpat quam ut erat consequat blandit sagittis et nisl. Sed laoreet metus quis dignissim auctor. Nam sit amet odio sed sapien dignissim varius. Duis pellentesque vulputate diam, egestas lobortis neque vestibulum eu.</p>
         <div class="linkdiv">
             <a href="assignments.php">Assignments</a>
             <img src="images/code.png" alt="codeicon" class="icon">
