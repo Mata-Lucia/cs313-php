391bc4c7b844b7d7dabeebee66d420d959f2b553
luciamata5 <mat18013@byui.edu>
2019-09-24 13:39:28 -0600

    Testing

--- web/index.php
+++ web/index.php
@@ -3,3 +3,64 @@
   phpinfo();
 
 ?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link type="text/css" rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <title>CS 313 Lucia Mata</title>
+</head>
+<body>
+    <header>
+           <!--Navbar-->
+           <nav class="navbar navbar-light teal lighten-4">
+
+            <!-- Navbar brand -->
+            <a class="navbar-brand" href="#">Menu</a>
+
+            <!-- Collapse button -->
+            <button class="navbar-toggler toggler-example" type="button" data-toggle="collapse" data-target="#navbarSupportedContent8" aria-controls="navbarSupportedContent8" aria-expanded="false" aria-label="Toggle navigation"><span class="dark-teal-text"><i class="fas fa-bars fa-1x"></i></span></button>
+
+            <!-- Collapsible content -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent8">
+
+            <!-- Links -->
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                <a class="nav-link waves-effect waves-light" href="index.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link waves-effect waves-light" href="assignments.php">Assignments</a>
+                </li>
+            </ul>
+            <!-- Links -->
+
+            </div>
+            <!-- Collapsible content -->
+
+            </nav>
+
+            <!--/.Navbar-->
+    </header>
+    <main>
+        <h1>Hello</h1>
+        <h2>My name is Lucia</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse scelerisque vulputate interdum. Nam accumsan tincidunt ligula at egestas. Donec eget metus at magna lacinia accumsan. Donec posuere lacus erat, eu sodales lorem scelerisque ac. Pellentesque tempus lacus nibh, a vehicula turpis mattis eget. Mauris varius rhoncus libero, quis faucibus est. In augue sem, tempor non rutrum consectetur, porta at est. Vivamus id urna ut sem rhoncus convallis. Duis et pulvinar enim.
+
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nullam cursus mollis leo pharetra pulvinar. Donec eu augue vitae eros lacinia tincidunt. Vivamus pulvinar erat sed consectetur maximus. Suspendisse egestas neque eu urna gravida finibus. Morbi volutpat quam ut erat consequat blandit sagittis et nisl. Sed laoreet metus quis dignissim auctor. Nam sit amet odio sed sapien dignissim varius. Duis pellentesque vulputate diam, egestas lobortis neque vestibulum eu.</p>
+        <img src="images/lucia.png" alt="lucia">
+        <div>
+            <a href="assignments.php">Assignments</a>
+            <img src="images/code.png" alt="codeicon">
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+    <script src="jquery.min.js"></script>
+    <script src="bootstrap.min.js"></script>
+</body>
+</html>
