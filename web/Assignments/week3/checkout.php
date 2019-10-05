5b63c4ffc81f33f479cb4396767c41d70b0d385f
luciamata5 <mat18013@byui.edu>
2019-10-05 13:53:42 -0600

    update

--- web/Assignments/week3/checkout.php
+++ web/Assignments/week3/checkout.php
@@ -12,12 +12,12 @@ session_start();
     <h1>Check Out</h1>
     <p>Please enter your address:</p>
     <form action="confirmation.php" method="post">
-        <label for="name">Name</label><input type="text" name="name">
-        <label for="street">Street</label><input type="text" name="street">
-        <label for="city">City</label><input type="text" name="city">
-        <label for="state">State</label><input type="text" name="state">
-        <label for="zip">Zip Code</label><input type="text" name="zip">
-        <input type="submit" name="submit" value="Submit">
+        <label for="name">Name</label><input type="text" name="name"><br>
+        <label for="street">Street</label><input type="text" name="street"><br>
+        <label for="city">City</label><input type="text" name="city"><br>
+        <label for="state">State</label><input type="text" name="state"><br>
+        <label for="zip">Zip Code</label><input type="text" name="zip"><br>
+        <input type="submit" name="submit" value="Complete Purchase">
     </form>
     <a href="cart.php">Go Back to Cart</a>
 </body>
