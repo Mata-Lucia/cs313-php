e096aa83d7cd008d8b8ae015071af8aafcd8de9c
luciamata5 <mat18013@byui.edu>
2019-10-05 14:12:14 -0600

    update

--- web/Assignments/week3/confirmation.php
+++ web/Assignments/week3/confirmation.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+
+$name = htmlspecialchars($_REQUEST["name"]);
+$street = htmlspecialchars($_REQUEST["address"]);
+$city = htmlspecialchars($_REQUEST["city"]);
+$state = htmlspecialchars($_REQUEST["state"]);
+$zip = htmlspecialchars($_REQUEST["zip"]);
+
 ?>
 
 <head>
@@ -10,5 +17,25 @@ session_start();
 </head>
 <body>
     <h1>Confirmation</h1>
-   
+    <p>Thank you for shopping!<p>
+    <p>Your order:</p>
+    <?php
+    foreach ( $_SESSION["cart"] as $i ) {
+    ?>
+        <div>
+            <p><?php echo( $product[$_SESSION["cart"][$i]] ); ?></p>
+            <p><?php echo( $_SESSION["product"][$i] ); ?></p>
+            <img src=<?php echo( $_SESSION["image"][$i] ); ?> alt="productimage">
+            <p><?php echo( $_SESSION["price"][$i] ); ?></p>
+            <a href="?delete=<?php echo($i); ?>">Delete from cart</a>
+        </div>
+    <?php
+    }
+    ?>
+    <p>Will be mailed to:</p>
+    <p>Name: <?php echo $name; ?></p>
+    <p>Address: <?php echo $address; ?></p>
+    <p>City: <?php echo $city; ?></p>
+    <p>State: <?php echo $state; ?></p>
+    <p>Zip Code: <?php echo $zip; ?></p>
 </body>
