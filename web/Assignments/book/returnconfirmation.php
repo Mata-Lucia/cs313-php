b9d2d72bbf20ad1429d9b633e202dd74abf564f9
luciamata5 <mat18013@byui.edu>
2019-10-26 16:21:14 -0600

    update

--- web/Assignments/book/returnconfirmation.php
+++ web/Assignments/book/returnconfirmation.php
@@ -2,7 +2,7 @@
 
 ini_set('display_errors', 1);
 
-$book_id = htmlspecialchars($_POST['bookid']);
+$book_id = htmlspecialchars($_POST['id']);
 $content = htmlspecialchars($_POST['review']);
 
 require('dbConnect.php');
