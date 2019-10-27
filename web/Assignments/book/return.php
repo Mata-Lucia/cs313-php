1ce70f16dd28d6d6cc2852d0109ac417594f4a9a
luciamata5 <mat18013@byui.edu>
2019-10-26 22:17:56 -0600

    update

--- web/Assignments/book/return.php
+++ web/Assignments/book/return.php
@@ -39,7 +39,7 @@ session_start();
                 // Loop through the query results, outputing the options one by one
                 while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
                     $bookid = $row['bookID'];
-                echo '<option value="$bookid">'.$row['title'].'</option>';
+                echo '<option value="'.$bookid.'">'.$row['title'].'</option>';
                 }
 
                 echo '</select>';
